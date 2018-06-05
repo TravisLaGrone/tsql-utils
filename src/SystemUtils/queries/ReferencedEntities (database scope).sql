aea3c5e8ac4642d8316dc02659e17fdf7cbbdd28
tlagrone <tlagrone@trintech.com>
2018-06-05 15:59:06 -0700

    * Modified 	- ReferencedEntities (database scope).sql 		+ refactored some names for consistency with source 		+ refactored for cleanliness 	- ReferencingEntities (database scope).sql 		+ completed

--- src/SystemUtils/queries/ReferencedEntities (database scope).sql
+++ src/SystemUtils/queries/ReferencedEntities (database scope).sql
@@ -1,105 +1,93 @@
 WITH
     ref_obj AS (
         SELECT
-            @@SERVERNAME AS [referencing_server_name],
-            DB_NAME() AS [referencing_database_name],
-            sch.[name] AS [referencing_schema_name],
-            obj.name AS [referencing_entity_name],
-            CASE WHEN ref.[referencing_minor_id] = 0 THEN NULL ELSE ref_col.name END AS [referencing_minor_name],
-            [ref].[referenced_server_name],
-            [ref].[referenced_database_name],
-            [ref].[referenced_schema_name],
-            [ref].[referenced_entity_name],
-            [ref].[referenced_minor_name],
-            obj.type_desc AS [referencing_entity_class_desc],
-            CASE WHEN ref.[referencing_minor_id] = 0 THEN NULL ELSE 'COLUMN' END AS [referencing_minor_class_desc],
-            CASE ref.[referenced_class_desc]
-                WHEN 'OBJECT_OR_COLUMN' THEN
-                    ref_obj.[type_desc]
-                ELSE
-                    ref.[referenced_class_desc]
-            END AS [referenced_entity_class_desc],
-            CASE WHEN ref.[referenced_minor_name] IS NULL THEN NULL ELSE 'COLUMN' END AS [referenced_minor_class_desc],
-            [ref].[is_caller_dependent],
-            [ref].[is_ambiguous],
-            [ref].[is_selected],
-            [ref].[is_updated],
-            [ref].[is_select_all],
-            [ref].[is_all_columns_found]
-        FROM sys.objects obj
-            INNER JOIN sys.schemas sch
-                ON  sch.[schema_id] = obj.[schema_id]
-            CROSS APPLY sys.[dm_sql_referenced_entities](sch.name + '.' + obj.[name], 'OBJECT') ref
-            LEFT JOIN sys.objects ref_obj
-                ON  [ref_obj].[object_id] = ref.[referenced_id]
-            LEFT JOIN sys.columns ref_col
-                ON  ref_col.[object_id] = obj.[object_id]
-                AND ref_col.[column_id] = ref.[referencing_minor_id]
+            @@SERVERNAME AS referencing_server_name,
+            DB_NAME() AS referencing_database_name,
+            sch.[name] AS referencing_schema_name,
+            obj.[name] AS referencing_entity_name,
+            CASE WHEN ref.referencing_minor_id = 0 THEN NULL ELSE ref_col.[name] END AS referencing_minor_name,
+            ref.referenced_server_name,
+            ref.referenced_database_name,
+            ref.referenced_schema_name,
+            ref.referenced_entity_name,
+            ref.referenced_minor_name,
+            'OBJECT' AS referencing_class_desc,
+            obj.[type_desc] AS referencing_object_type_desc,
+            CASE WHEN ref.referencing_minor_id = 0 THEN NULL ELSE 'COLUMN' END AS referencing_minor_class_desc,
+            ref.referenced_class_desc AS referenced_class_desc,
+            ref.referenced_id,  -- temporary placeholder to compute [referenced_object_type_desc] later
+            ref.is_caller_dependent,
+            ref.is_ambiguous,
+            ref.is_selected,
+            ref.is_updated,
+            ref.is_select_all,
+            ref.is_all_columns_found
+        FROM sys.objects AS obj
+            INNER JOIN sys.schemas AS sch
+                ON  sch.[schema_id]             = obj.[schema_id]
+            CROSS APPLY sys.dm_sql_referenced_entities(sch.[name] + '.' + obj.[name], 'OBJECT') AS ref
+            LEFT JOIN sys.objects                   AS ref_obj
+                ON  ref_obj.[object_id]         = ref.referenced_id
+            LEFT JOIN sys.columns                   AS ref_col
+                ON  ref_col.[object_id]         = obj.[object_id]
+                AND ref_col.column_id         = ref.referencing_minor_id
     ),
     ref_db_ddl_trg AS (
         SELECT
-            @@SERVERNAME AS [referencing_server_name],
-            DB_NAME() AS [referencing_database_name],
-            NULL AS [referencing_schema_name],
-            trg.name AS [referencing_entity_name],
-            NULL AS [referencing_minor_name],
-            [ref].[referenced_server_name],
-            [ref].[referenced_database_name],
-            [ref].[referenced_schema_name],
-            [ref].[referenced_entity_name],
-            [ref].[referenced_minor_name],
-            trg.[type_desc] AS [referencing_entity_class_desc],
-            NULL AS [referencing_minor_class_desc],
-            CASE ref.[referenced_class_desc]
-                WHEN 'OBJECT_OR_COLUMN' THEN
-                    ref_obj.[type_desc]
-                ELSE
-                    ref.[referenced_class_desc]
-            END AS [referenced_entity_class_desc],
-            CASE WHEN ref.[referenced_minor_name] IS NULL THEN NULL ELSE 'COLUMN' END AS [referenced_minor_class_desc],
-            [ref].[is_caller_dependent],
-            [ref].[is_ambiguous],
-            [ref].[is_selected],
-            [ref].[is_updated],
-            [ref].[is_select_all],
-            [ref].[is_all_columns_found]
-        FROM sys.triggers trg
-            CROSS APPLY sys.[dm_sql_referenced_entities](trg.[name], 'DATABASE_DDL_TRIGGER') ref
-            LEFT JOIN sys.objects ref_obj
-                ON  [ref_obj].[object_id] = ref.[referenced_id]
-        WHERE trg.[parent_class] = 0  -- database (for DDL)
+            @@SERVERNAME AS referencing_server_name,
+            DB_NAME() AS referencing_database_name,
+            NULL AS referencing_schema_name,
+            trg.[name] AS referencing_entity_name,
+            NULL AS referencing_minor_name,
+            ref.referenced_server_name,
+            ref.referenced_database_name,
+            ref.referenced_schema_name,
+            ref.referenced_entity_name,
+            ref.referenced_minor_name,
+            'DATABASE_DDL_TRIGGER' AS referencing_class_desc,
+            trg.[type_desc] AS referencing_object_type_desc,
+            NULL AS referencing_minor_class_desc,
+            ref.referenced_class_desc AS referenced_class_desc,
+            ref.referenced_id,  -- temporary placeholder to compute [referenced_object_type_desc] later
+            ref.is_caller_dependent,
+            ref.is_ambiguous,
+            ref.is_selected,
+            ref.is_updated,
+            ref.is_select_all,
+            ref.is_all_columns_found
+        FROM sys.triggers AS trg
+            CROSS APPLY sys.dm_sql_referenced_entities(trg.[name], 'DATABASE_DDL_TRIGGER') AS ref
+            LEFT JOIN sys.objects                   AS ref_obj
+                ON  ref_obj.[object_id]         = ref.referenced_id
+        WHERE trg.parent_class = 0  -- database (for DDL)
     ),
     ref_srv_ddl_trg AS (
         SELECT
-            @@SERVERNAME AS [referencing_server_name],
-            NULL AS [referencing_database_name],
-            NULL AS [referencing_schema_name],
-            trg.name AS [referencing_entity_name],
-            NULL AS [referencing_minor_name],
-            [ref].[referenced_server_name],
-            [ref].[referenced_database_name],
-            [ref].[referenced_schema_name],
-            [ref].[referenced_entity_name],
-            [ref].[referenced_minor_name],
-            trg.[type_desc] AS [referencing_entity_class_desc],
-            NULL AS [referencing_minor_class_desc],
-            CASE ref.[referenced_class_desc]
-                WHEN 'OBJECT_OR_COLUMN' THEN
-                    ref_obj.[type_desc]
-                ELSE
-                    ref.[referenced_class_desc]
-            END AS [referenced_entity_class_desc],
-            CASE WHEN ref.[referenced_minor_name] IS NULL THEN NULL ELSE 'COLUMN' END AS [referenced_minor_class_desc],
-            [ref].[is_caller_dependent],
-            [ref].[is_ambiguous],
-            [ref].[is_selected],
-            [ref].[is_updated],
-            [ref].[is_select_all],
-            [ref].[is_all_columns_found]
-        FROM sys.[server_triggers] trg
-            CROSS APPLY sys.[dm_sql_referenced_entities](trg.[name], 'SERVER_DDL_TRIGGER') ref
-            LEFT JOIN sys.objects ref_obj
-                ON  [ref_obj].[object_id] = ref.[referenced_id]
+            @@SERVERNAME AS referencing_server_name,
+            NULL AS referencing_database_name,
+            NULL AS referencing_schema_name,
+            trg.[name] AS referencing_entity_name,
+            NULL AS referencing_minor_name,
+            ref.referenced_server_name,
+            ref.referenced_database_name,
+            ref.referenced_schema_name,
+            ref.referenced_entity_name,
+            ref.referenced_minor_name,
+            'SERVER_DDL_TRIGGER' AS referencing_class_desc,
+            trg.[type_desc] AS referencing_object_type_desc,
+            NULL AS referencing_minor_class_desc,
+            ref.referenced_class_desc AS referenced_class_desc,
+            ref.referenced_id,  -- temporary placeholder to compute [referenced_object_type_desc] later
+            ref.is_caller_dependent,
+            ref.is_ambiguous,
+            ref.is_selected,
+            ref.is_updated,
+            ref.is_select_all,
+            ref.is_all_columns_found
+        FROM sys.server_triggers AS trg
+            CROSS APPLY sys.dm_sql_referenced_entities(trg.[name], 'SERVER_DDL_TRIGGER') AS ref
+            LEFT JOIN sys.objects AS ref_obj
+                ON  ref_obj.[object_id] = ref.referenced_id
     ),
     ref AS (
         SELECT * FROM [ref_obj]
@@ -108,16 +96,40 @@ WITH
         UNION ALL
         SELECT * FROM [ref_srv_ddl_trg]
     )
-SELECT *
+SELECT
+    ref.referencing_server_name,
+    ref.referencing_database_name,
+    ref.referencing_schema_name,
+    ref.referencing_entity_name,
+    ref.referencing_minor_name,
+    ref.referenced_server_name,
+    ref.referenced_database_name,
+    ref.referenced_schema_name,
+    ref.referenced_entity_name,
+    ref.referenced_minor_name,
+    ref.referencing_class_desc,
+    ref.referencing_object_type_desc,
+    ref.referencing_minor_class_desc,
+    ref.referenced_class_desc,
+    CASE WHEN ref.referenced_class_desc = 'OBJECT_OR_COLUMN' THEN ref_obj.[type_desc] ELSE NULL END AS referenced_object_type_desc,
+    CASE WHEN ref.referenced_minor_name IS NOT NULL          THEN 'COLUMN'            ELSE NULL END AS referenced_minor_class_desc,
+    ref.is_caller_dependent,
+    ref.is_ambiguous,
+    ref.is_selected,
+    ref.is_updated,
+    ref.is_select_all,
+    ref.is_all_columns_found AS is_all_columns_found_entity
 FROM ref
+    LEFT JOIN sys.objects AS ref_obj
+        ON  ref_obj.[object_id] = ref.referenced_id
 ORDER BY
-    ref.[referencing_server_name],
-    ref.[referencing_database_name],
-    ref.[referencing_schema_name],
-    ref.[referencing_entity_name],
-    ref.[referencing_minor_name],
-    ref.[referenced_server_name],
-    ref.[referenced_database_name],
-    ref.[referenced_schema_name],
-    ref.[referenced_entity_name],
-    ref.[referenced_minor_name]
+    ref.referencing_server_name,
+    ref.referencing_database_name,
+    ref.referencing_schema_name,
+    ref.referencing_entity_name,
+    ref.referencing_minor_name,
+    ref.referenced_server_name,
+    ref.referenced_database_name,
+    ref.referenced_schema_name,
+    ref.referenced_entity_name,
+    ref.referenced_minor_name
--- src/SystemUtils/queries/ReferencingEntities (database scope).sql
+++ src/SystemUtils/queries/ReferencingEntities (database scope).sql
@@ -1,42 +1,144 @@
--- OBJECT
--- TYPE
--- XML_SCHEMA_COLLECTION
--- PARTITION_FUNCTION
-
 WITH
     ref_obj AS (
         SELECT
-            @@SERVERNAME AS [referenced_server_name],
-            DB_NAME() AS [referenced_database_name],
-            [sch].[name] AS [referenced_schema_name],
-            [obj].[name] AS [referenced_entity_name],
-            @@SERVERNAME AS [referencing_server_name],
-            DB_NAME() AS [referencing_database_name],
-            [ref].[referencing_schema_name],
-            [ref].[referencing_entity_name],
-            [obj].[type_desc] AS [referenced_entity_class_desc],
-            [ref].[referencing_class_desc] AS [referencing_entity_class_desc],  -- FIXME (do for other query too)
-            [ref].[is_caller_dependent]
-        FROM [sys].[objects] obj
-            INNER JOIN [sys].[schemas] sch
-                ON  [sch].[schema_id] = [obj].[schema_id]
-            CROSS APPLY [sys].[dm_sql_referencing_entities]([sch].[name] + '.' + [obj].[name], 'OBJECT') AS ref
+            @@SERVERNAME AS [server_name],
+            DB_NAME() AS [database_name],
+            sch.[name] AS referenced_schema_name,
+            obj.[name] AS referenced_entity_name,
+            ref.referencing_schema_name,
+            ref.referencing_entity_name,
+            'OBJECT' AS referenced_class_desc,
+            ref.referencing_class_desc,
+            obj.[type_desc] AS referenced_object_type_desc,
+            ref.referencing_id,  -- temporary placeholder to compute [referencing_object_type_desc] later
+            ref.is_caller_dependent
+        FROM sys.objects AS obj
+            INNER JOIN sys.schemas AS sch
+                ON  sch.[schema_id] = obj.[schema_id]
+            CROSS APPLY sys.dm_sql_referencing_entities(sch.[name] + '.' + obj.[name], 'OBJECT') AS ref
+            LEFT JOIN sys.objects AS ref_obj
+                ON  ref_obj.[object_id] = ref.referencing_id
+            LEFT JOIN sys.triggers          AS ref_trg
+                ON  ref_trg.[object_id]     = ref.referencing_id
+            LEFT JOIN sys.server_triggers   AS ref_srv_trg
+                ON  ref_srv_trg.[object_id] = ref.referencing_id
     ),
-    ref_type AS (
+    ref_typ AS (
         SELECT
-            @@SERVERNAME AS [referenced_server_name],
-            DB_NAME() AS [referenced_database_name],
-            [sch].[name] AS [referenced_schema_name],
-            [typ].[name] AS [referenced_entity_name],
-            @@SERVERNAME AS [referencing_server_name],
-            DB_NAME() AS [referencing_database_name],
-            [ref].[referencing_schema_name],
-            [ref].[referencing_entity_name],
-            'TYPE' AS [referenced_entity_class_desc],
-            [ref].[referencing_class_desc] AS [referencing_entity_class_desc],  -- FIXME (do for other query too)
-            [ref].[is_caller_dependent]
-        FROM [sys].[types] typ
-            INNER JOIN [sys].[schemas] sch
-                ON  [sch].[schema_id] = [typ].[schema_id]
-            CROSS APPLY [sys].[dm_sql_referencing_entities]([sch].[name] + '.' + [typ].[name], 'OBJECT') AS ref
+            @@SERVERNAME AS [server_name],
+            DB_NAME() AS [database_name],
+            sch.[name] AS referenced_schema_name,
+            typ.[name] AS referenced_entity_name,
+            ref.referencing_schema_name,
+            ref.referencing_entity_name,
+            'TYPE' AS referenced_class_desc,
+            ref.referencing_class_desc,
+            NULL AS referenced_object_type_desc,
+            ref.referencing_id,  -- temporary placeholder to compute [referencing_object_type_desc] later
+            ref.is_caller_dependent
+        FROM sys.types typ
+            INNER JOIN sys.schemas AS sch
+                ON  sch.[schema_id] = typ.[schema_id]
+            CROSS APPLY sys.dm_sql_referencing_entities(sch.[name] + '.' + typ.[name], 'TYPE') AS ref
+            LEFT JOIN sys.objects AS ref_obj
+                ON  ref_obj.[object_id] = ref.referencing_id
+            LEFT JOIN sys.triggers          AS ref_trg
+                ON  ref_trg.[object_id]     = ref.referencing_id
+            LEFT JOIN sys.server_triggers   AS ref_srv_trg
+                ON  ref_srv_trg.[object_id] = ref.referencing_id
     ),
+    ref_xsc AS (
+        SELECT
+            @@SERVERNAME AS [server_name],
+            DB_NAME() AS [database_name],
+            sch.[name] AS referenced_schema_name,
+            xsc.[name] AS referenced_entity_name,
+            ref.referencing_schema_name,
+            ref.referencing_entity_name,
+            'XML_SCHEMA_COLLECTION' AS referenced_class_desc,
+            ref.referencing_class_desc,
+            NULL AS referenced_object_type_desc,
+            ref.referencing_id,  -- temporary placeholder to compute [referencing_object_type_desc] later
+            ref.is_caller_dependent
+        FROM sys.xml_schema_collections xsc
+            INNER JOIN sys.schemas AS sch
+                ON  sch.[schema_id] = xsc.[schema_id]
+            CROSS APPLY sys.dm_sql_referencing_entities(sch.[name] + '.' + xsc.[name], 'XML_SCHEMA_COLLECTION') AS ref
+            LEFT JOIN sys.objects           AS ref_obj
+                ON  ref_obj.[object_id]     = ref.referencing_id
+            LEFT JOIN sys.triggers          AS ref_trg
+                ON  ref_trg.[object_id]     = ref.referencing_id
+            LEFT JOIN sys.server_triggers   AS ref_srv_trg
+                ON  ref_srv_trg.[object_id] = ref.referencing_id
+    ),
+    ref_par AS (
+        SELECT
+            @@SERVERNAME AS [server_name],
+            DB_NAME() AS [database_name],
+            NULL AS referenced_schema_name,
+            par.[name] AS referenced_entity_name,
+            ref.referencing_schema_name,
+            ref.referencing_entity_name,
+            'PARTITION_FUNCTION' AS referenced_class_desc,
+            ref.referencing_class_desc,
+            NULL AS referenced_object_type_desc,
+            ref.referencing_id,  -- temporary placeholder to compute [referencing_object_type_desc] later
+            ref.is_caller_dependent
+        FROM sys.partition_functions par
+            CROSS APPLY sys.dm_sql_referencing_entities(par.[name], 'PARTITION_FUNCTION') AS ref
+            LEFT JOIN sys.objects AS ref_obj
+                ON  ref_obj.[object_id] = ref.referencing_id
+            LEFT JOIN sys.triggers          AS ref_trg
+                ON  ref_trg.[object_id]     = ref.referencing_id
+            LEFT JOIN sys.server_triggers   AS ref_srv_trg
+                ON  ref_srv_trg.[object_id] = ref.referencing_id
+    ),
+    ref AS (
+        SELECT * FROM ref_obj
+        UNION ALL
+        SELECT * FROM ref_typ
+        UNION ALL
+        SELECT * FROM ref_xsc
+        UNION ALL
+        SELECT * FROM ref_par
+    )
+SELECT
+    ref.[server_name],
+    ref.[database_name],
+    ref.referenced_schema_name,
+    ref.referenced_entity_name,
+    ref.referencing_schema_name,
+    ref.referencing_entity_name,
+    ref.referenced_class_desc,
+    CASE ref.referencing_class_desc
+        WHEN 'OBJECT_OR_COLUMN' THEN
+            'OBJECT'
+        ELSE
+            ref.referencing_class_desc
+    END AS referencing_class_desc,
+    ref.referenced_object_type_desc,
+    CASE ref.referencing_class_desc
+        WHEN 'OBJECT_OR_COLUMN' THEN
+            ref_obj.[type_desc]
+        WHEN 'DATABASE_DDL_TRIGGER' THEN
+            ref_trg.[type_desc]
+        WHEN 'SERVER_DDL_TRIGGER' THEN
+            ref_srv_trg.[type_desc]
+        ELSE
+            NULL  -- this should never happen
+    END AS referencing_object_type_desc,
+    ref.is_caller_dependent
+FROM ref
+    LEFT JOIN sys.objects           AS ref_obj
+        ON  ref_obj.[object_id]     = ref.referencing_id
+    LEFT JOIN sys.triggers          AS ref_trg
+        ON  ref_trg.[object_id]     = ref.referencing_id
+    LEFT JOIN sys.server_triggers   AS ref_srv_trg
+        ON  ref_srv_trg.[object_id] = ref.referencing_id
+ORDER BY
+    ref.[server_name],
+    ref.[database_name],
+    ref.[referenced_schema_name],
+    ref.[referenced_entity_name],
+    ref.[referencing_schema_name],
+    ref.[referencing_entity_name]
