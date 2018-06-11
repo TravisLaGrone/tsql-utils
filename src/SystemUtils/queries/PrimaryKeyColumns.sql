d988ef35a3f906aff3e90eca6932e8455339f89f
tlagrone <tlagrone@trintech.com>
2018-06-11 02:18:09 -0500

    Improved style in "PrimaryKeyColumns" query for semantic comprehension. - Renamed `con` alias of to `key_con` for "sys.key_constraints." - Moved up the join on "sys.key_constraints" to make more sense in the 'flow'.

--- src/SystemUtils/queries/PrimaryKeyColumns.sql
+++ src/SystemUtils/queries/PrimaryKeyColumns.sql
@@ -1,7 +1,7 @@
 SELECT
     sch.[name] AS [schema_name],
     obj.[name] AS [object_name],
-    con.[name] AS primary_key_name,
+    key_con.[name] AS primary_key_name,
     col.[name] AS column_name
 FROM sys.columns AS col
     INNER JOIN sys.index_columns AS idx_col
@@ -10,18 +10,18 @@ FROM sys.columns AS col
     INNER JOIN sys.indexes AS idx
     	ON	idx.[object_id] = idx_col.[object_id]
         AND idx.index_id  = idx_col.index_id
+    INNER JOIN sys.key_constraints AS key_con
+        ON  key_con.[object_id] = idx.[object_id]
+        AND key_con.unique_index_id = idx.index_id
     INNER JOIN sys.objects AS obj
     	ON	obj.[object_id] = idx.[object_id]
     INNER JOIN sys.schemas AS sch
     	ON	sch.[schema_id] = obj.[schema_id]
-    INNER JOIN sys.key_constraints AS con
-        ON  con.[object_id] = idx.[object_id]
-        AND con.unique_index_id = idx.index_id
 WHERE
     idx.is_primary_key = 1
     AND sch.[name] <> 'sys'
 ORDER BY
     sch.[name],
     obj.[name],
-    con.[name],
+    key_con.[name],
     idx_col.key_ordinal
